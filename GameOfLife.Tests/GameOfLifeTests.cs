03f330bbd25bb0e71fa5d393f977395d3e10f2c7
Kjell Otto <otto.kjell@gmail.com>
2014-07-26 16:44:57 +0100

    initial commit

---
+++ GameOfLife.Tests/GameOfLifeTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+using GameOfLife;
+
+namespace GameOfLife.Tests
+{
+    [TestFixture]
+    public class GameOfLifeTests
+    {
+        private const int YSize = 2;
+        private const int XSize = 2;
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        [TestCase(int.MaxValue, 0)]
+        [TestCase(0, int.MaxValue)]
+        public void GetCell_When_X_Or_Y_Out_Of_Bounds(int x, int y)
+        {
+            // arrange
+            var land = new [,]
+            {
+                {false, false},
+                {false, false}
+            };
+
+            // act
+            // assert
+            Assert.That(() => GameOfLife.GetCell(x, y, land), Throws.Nothing);
+        }
+
+        [Test]
+        public void GetCell_Returns_One_For_Life_And_Zero_For_Dead_Cell()
+        {
+            // arrange
+            var land = new[,]
+            {
+                {true, false},
+                {true, false}
+            };
+
+            // act
+            // assert
+            Assert.That(GameOfLife.GetCell(0, 0, land), Is.EqualTo(1));
+            Assert.That(GameOfLife.GetCell(0, 1, land), Is.EqualTo(1));
+            Assert.That(GameOfLife.GetCell(1, 0, land), Is.EqualTo(0));
+            Assert.That(GameOfLife.GetCell(1, 1, land), Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(10, 1)]
+        [TestCase(1, 10)]
+        [TestCase(10, 10)]
+        public void GetDimensions_Resolves_Dimensions(int xSize, int ySize)
+        {
+            // arrange
+            var gof = new GameOfLife(xSize, ySize);
+
+            // act
+            var result = gof.GetDimensions();
+
+            // assert
+            Assert.That(result.XSize, Is.EqualTo(xSize));
+            Assert.That(result.YSize, Is.EqualTo(ySize));
+        }
+    }
+}
---
+++ GameOfLife/GameOfLife.cs
@@ -0,0 +1,56 @@
+﻿namespace GameOfLife
+{
+    public class GameOfLife
+    {
+        public const int XDimension = 1;
+        public const int YDimension = 0;
+        private readonly bool[,] _land;
+
+        private readonly int _xSize;
+        private readonly int _ySize;
+
+        public GameOfLife(int xSize, int ySize)
+            : this(new bool[ySize, xSize])
+        {
+        }
+
+        public GameOfLife(bool[,] land)
+        {
+            _xSize = land.GetLength(XDimension);
+            _ySize = land.GetLength(YDimension);
+            _land = land;
+        }
+
+        //public bool[,] NextGeneration()
+        //{
+        //    for (int x = 0; x < _xSize; x++)
+        //    {
+        //        for (int y = 0; y < _ySize; y++)
+        //        {
+        //            var cellNeighbours = CountNeighbours(x,y,_land);
+        //        }
+        //    }
+        //}
+
+        //public static int CountNeighbours(int x, int y, bool[,] land)
+        //{
+        //    var result = 0;
+        //    return GetCell(x-1,y-1,land) 
+        //}
+
+        public Dimensions GetDimensions()
+        {
+            return new Dimensions(_land.GetLength(XDimension), _land.GetLength(YDimension));
+        }
+
+        public static int GetCell(int x, int y, bool[,] land)
+        {
+            if (x < 0 || x > land.GetLength(XDimension) - 1 || y < 0 || y > land.GetLength(YDimension))
+            {
+                return 0;
+            }
+
+            return land[y, x] ? 1 : 0;
+        }
+    }
+}
